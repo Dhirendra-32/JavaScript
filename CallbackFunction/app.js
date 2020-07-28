0cbf3501aa3dfd2001499a738d6b9f278ce465dd
Dhirendra-32 <tdhirendra32@gmail.com>
2020-07-28 11:05:03 +0530

    call back best example

--- CallbackFunction/app.js
+++ CallbackFunction/app.js
@@ -4,32 +4,10 @@ const posts = [
   { title: "Post two", body: "this is body of post two " },
 ];
 
-// function createPost(post) {
-//   setTimeout(function () {
-//     post.push(post);
-//   }, 2000);
-// }
-
-// function getPost() {
-//   setTimeout(function () {
-//     let output = "";
-
-//     posts.forEach(function (post) {
-//       output += `<li>${post.title}</li>`;
-//     });
-
-//     document.body.innerHTML = output;
-//   }, 1000);
-// }
-
-// createPost({ title: "post 3", body: "This is post three" });
-
-// getPost();
-
-function createPost(post, callback) {
+function createPost(post) {
   setTimeout(function () {
     posts.push(post);
-    callback();
+    console.log("third element has been added");
   }, 2000);
 }
 
@@ -42,7 +20,32 @@ function getPost() {
     });
 
     document.body.innerHTML = output;
+    console.log("Element gas been fetched");
   }, 1000);
 }
 
-createPost({ title: "Post Three", body: "This is post three" }, getPost);
+createPost({ title: "post 3", body: "This is post three" });
+
+getPost();
+
+// function createPost(post, callback) {
+//   setTimeout(function () {
+//     posts.push(post);
+//     console.log("third element pushed");
+//     callback();
+//   }, 1000);
+// }
+
+// function getPost() {
+//   setTimeout(function () {
+//     let output = "";
+
+//     posts.forEach(function (post) {
+//       output += `<li>${post.title}</li>`;
+//     });
+//     document.body.innerHTML = output;
+//     console.log("All element has been fetched");
+//   }, 1000);
+// }
+
+// createPost({ title: "Post Three", body: "This is post three" }, getPost);
